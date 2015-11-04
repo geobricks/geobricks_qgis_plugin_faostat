167bb89f0f9641a9f262d609f83d7e16b6daf1a5
Kalimaha <guido.barbaglia@gmail.com>
2015-11-04 20:03:25 +0100

    Italian translations.

---
+++ i18n/geobricks_qgis_plugin_faostat.pro
@@ -0,0 +1,6 @@
+FORMS = ../geobricks_qgis_plugin_faostat_dialog_base.ui
+
+SOURCES = ../geobricks_qgis_plugin_faostat.py \
+          ../geobricks_qgis_plugin_faostat.py
+
+TRANSLATIONS = geobricks_qgis_plugin_faostat_it.ts
---
+++ i18n/geobricks_qgis_plugin_faostat_it.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>geobricks_qgis_plugin_faostat</name>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat.py" line="174"/>
+        <source>&amp;FAOSTAT Data Downloader</source>
+        <translation>Acquisizione Dati FAOSTAT</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat.py" line="164"/>
+        <source>FAOSTAT Data Downloader</source>
+        <translation>Acquisizione Dati FAOSTAT</translation>
+    </message>
+</context>
+<context>
+    <name>geobricks_qgis_plugin_faostatDialogBase</name>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="14"/>
+        <source>FAOSTAT Data Downloader</source>
+        <translation>Acquisizione Dati FAOSTAT</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="26"/>
+        <source>Domain</source>
+        <translation>Dominio</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="39"/>
+        <source>Element</source>
+        <translation>Elemento</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="52"/>
+        <source>Item</source>
+        <translation>Articolo</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="66"/>
+        <source>Please select a domain...</source>
+        <translation>Selezionare un dominio...</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="71"/>
+        <source>Production: Crops</source>
+        <translation>Produzione: Coltivazioni</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="76"/>
+        <source>Production: Crops Processed</source>
+        <translation>Produzione: Coltivazioni Processate</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="81"/>
+        <source>Production: Live Animals</source>
+        <translation>Produzione: Animali Vivi</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="86"/>
+        <source>Production: Livestock Primary</source>
+        <translation>Produzione: Animali Processati</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="91"/>
+        <source>Production: Livestock Processed</source>
+        <translation>Produzione: Bestiame Processato</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="96"/>
+        <source>Production: Production Indices</source>
+        <translation>Produzione: Indici di Produzione</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="101"/>
+        <source>Production: Vaue of Agricultural Production</source>
+        <translation>Produzione: Valore della Produzione Agricola</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="116"/>
+        <source>Please select an element...</source>
+        <translation>Selezionare un elemento...</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="121"/>
+        <source>Area Harvested</source>
+        <translation>Area di Raccolto</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="126"/>
+        <source>Yield</source>
+        <translation>Resa</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="131"/>
+        <source>Production Quantity</source>
+        <translation>Quantitá Prodotta</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="136"/>
+        <source>Seed</source>
+        <translation>Semi</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="151"/>
+        <source>Please select an item...</source>
+        <translation>Selezionare un articolo...</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="156"/>
+        <source>Apples</source>
+        <translation>Mele</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="161"/>
+        <source>Wheat</source>
+        <translation>Grano</translation>
+    </message>
+    <message>
+        <location filename="../geobricks_qgis_plugin_faostat_dialog_base.ui" line="175"/>
+        <source>Download Data</source>
+        <translation>Acquisizione Dati</translation>
+    </message>
+</context>
+</TS>
